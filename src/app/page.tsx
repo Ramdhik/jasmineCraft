786b1da6477ff32a09487e39264338beb605a4fa
Ramadhika123 <ramadhikadarma0607@gmail.com>
2025-06-03 16:03:02 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+
+      <h1 className="text-3xl font-bold">Jasmin IdCraft</h1>
+      <p className="text-gray-600">
+        Jasmin IdCraft is a platform for creating and sharing your own
+        digital art.
+      </p>
+    </div>
+  )
+}
